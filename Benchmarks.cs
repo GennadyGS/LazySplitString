899d5815f91dfae31b4262cae2a8470c245a7727
Gennadii Saltyshchak <GennadyGS@gmail.com>
2024-02-27 08:34:40 +0200

    Add project files.

---
+++ Benchmarks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace LazySplitString;
+
+[MediumRunJob]
+public class Benchmarks
+{
+    private const string Input = "Column1,Column2,Column3";
+    
+    private readonly IReadOnlyCollection<string> _precalculated = Calculate();
+
+    private readonly Lazy<IReadOnlyCollection<string>> _lazy = new(Calculate);
+
+    [Benchmark]
+    public void ReadPrecalculated()
+    {
+        var result = _precalculated;
+    }
+
+    [Benchmark]
+    public void ReadLazy()
+    {
+        var result = _lazy.Value;
+    }
+
+    [Benchmark]
+    public void Recalculate()
+    {
+        var result = Calculate();
+    }
+
+    private static IReadOnlyCollection<string> Calculate() => Input.Split(',');
+}
